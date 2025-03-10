58f7d01d729708b44601da49361476f0d5925e1f
copengocngheck <nguyenthibalee1705@gmail.com>
2025-03-10 16:26:51 +0700

    cap nhat hello

---
+++ app/Http/Controllers/helloController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class helloController extends Controller
+{
+    function bale()
+    {
+        return view('hello');
+    }
+
+}
---
+++ resources/views/hello.php
@@ -0,0 +1,3 @@
+<h1>Xin chào người đẹp</h1>
+<h2>Cơm nước gì chưa người đẹp</h2>
+<img src="https://cellphones.com.vn/sforum/wp-content/uploads/2024/01/anh-meme-57.jpg">
